c3dd4a94f556f635e8d0c8985e37adaf3763da79
Natasha Malhan <natashamalhan@natashas-air.lan>
2023-06-21 13:22:34 -0700

    index

--- index.js
+++ index.js
@@ -8,3 +8,50 @@ const fs = require("fs");
 const path = require("path")
 const generateHTML = require('./util/generateHtml.js');
 const employees = []
+
+const questions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is the employee's name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the employee's ID?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the employee's email?"
+    },
+    {
+        type: "list",
+        name: "role",
+        message: "What is the employee's role?",
+        choices: ["Manager", "Engineer", "Intern"]
+    },
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the manager's office number?",
+        when: (answers) => answers.role === "Manager"
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is the engineer's GitHub username?",
+        when: (answers) => answers.role === "Engineer"
+    },
+    {
+        type: "input",
+        name: "school",
+        message: "What is the intern's school?",
+        when: (answers) => answers.role === "Intern"
+    },
+    {
+        type: "confirm",
+        name: "addEmployee",
+        message: "Would you like to add another employee?"
+    }
+]
