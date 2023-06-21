7f115bd8bb731402c57fee36a157aefa072b2a16
Natasha Malhan <natashamalhan@natashas-air.lan>
2023-06-21 13:10:49 -0700

    const

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const Employee = require("./lib/Employee.js");
 const Manager = require("./lib/Manager.js");
 const Engineer = require("./lib/Engineer.js");
 const Intern = require("./lib/Intern.js");
+
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path")
+const generateHTML = require('./util/generateHtml.js');
+const employees = []
